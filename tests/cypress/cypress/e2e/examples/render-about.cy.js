ac21f7ebc747604d7c4d0573a93d82b49ef47f49
Louis Law <louiscklaw@users.noreply.github.com>
2023-10-09 19:27:43 +0800

    update,

--- tests/cypress/cypress/e2e/examples/render-about.cy.js
+++ tests/cypress/cypress/e2e/examples/render-about.cy.js
@@ -9,8 +9,26 @@ context('Actions', () => {
     cy.log('helloworld');
   });
 
-  it('portfolio-desktop-render', () => {
+  it('portfolio-4k-ultra-hd-render', () => {
+    cy.viewport(3840, 2160);
+    cy.wait(1 * 1000);
+    cy.screenshot({ capture: 'runner', overwrite: true });
+  });
+
+  it('portfolio-2k-quad-hd-render', () => {
+    cy.viewport(2560, 1440);
+    cy.wait(1 * 1000);
+    cy.screenshot({ capture: 'runner', overwrite: true });
+  });
+
+  it('portfolio-full-hd-render', () => {
     cy.viewport(1920, 1080);
+    cy.wait(1 * 1000);
+    cy.screenshot({ capture: 'runner', overwrite: true });
+  });
+
+  it('portfolio-hd-render', () => {
+    cy.viewport(1280, 720);
     cy.wait(1 * 1000);
     cy.screenshot({ capture: 'runner', overwrite: true });
   });
