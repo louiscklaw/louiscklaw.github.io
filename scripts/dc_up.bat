17bb12cdf371f62c3975192e7decaa05f646d005
louiscklaw <louiscklaw@users.noreply.github.com>
2023-10-22 20:01:08 +0800

    update,

--- scripts/dc_up.bat
+++ scripts/dc_up.bat
@@ -5,7 +5,7 @@ docker run -it ^
   -v D:\_workspace\louiscklaw.github.io:/app ^
   -w /app ^
   -v temp_node_modules:/app/node_modules ^
-  -p 3001 ^
+  -p 3001:3001 ^
   -p 8080:8080 ^
   --rm ^
   node:16-bullseye bash
